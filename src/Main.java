0e58ef30c966745af653f73d957d2c12dc829527
Aimer <2010914699@qq.com>
2024-06-29 20:00:59 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     // Main method
+    // This is the entry point of the program
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
