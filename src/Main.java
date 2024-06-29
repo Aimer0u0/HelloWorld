fea46aeb14b8e177e12e1499440920fbaa5607e3
Aimer <2010914699@qq.com>
2024-06-29 20:16:28 +0800

    hot-fix commit2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Print "Hello world!" to the console
         System.out.println("Hello world!");
+        System.out.println("Hello world!2");
     }
 }
