4aac7f5cf1ce96a15707950a96c2f87d05bef43c
Aimer <2010914699@qq.com>
2024-06-29 20:13:55 +0800

    hot-fix commit1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     // Main method
     // This is the entry point of the program
     public static void main(String[] args) {
+        // Print "Hello world!" to the console
         System.out.println("Hello world!");
     }
 }
